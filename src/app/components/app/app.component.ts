0ff96b243b682281a49fa2fd1f4a3dac6a3229ff
Rafael <rafaortiz0206@gmail.com>
2019-07-12 10:28:39 -0500

    1.3

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { Usuario, UserType } from 'src/app/others/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tutorial';
+
+
+user: Usuario[] =[{
+  ID: 1,
+  Nombre: "Rafael",
+  Apellidos: "Ortiz",
+  Nick: "rafael",
+  Email: "rafael",
+  Contraseña: "123",
+  Tipo: UserType.Administrador
+},{
+  ID: 2,
+  Nombre: "Dolmedes",
+  Apellidos: "Ortiz",
+  Nick: "rafael",
+  Email: "rafael",
+  Contraseña: "123",
+  Tipo: UserType.Cliente
+}]
+
+//Hacer casting
+
+user1: Usuario =<Usuario>{
+  ID: 3,
+  Nombre: "Dolmedes",
+  Apellidos: "Ortiz",
+  Nick: "rafael",
+  Email: "rafael",
+  Contraseña: "123",
+  Tipo: UserType.Cliente
 }
+
+mostrar():void{
+console.log(this.user1.Apellidos);
+}
+
+}
+
